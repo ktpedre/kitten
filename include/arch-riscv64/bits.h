22412143f51aa1c890fafb18e2522a4c0d8a4f3b
Nicholas Michael Gordon <nmgordo@sandia.gov>
2022-08-02 14:36:58 -0600

    Lost header didn't make it into other commits.

---
+++ include/arch-riscv64/bits.h
@@ -0,0 +1,21 @@
+#ifndef __ARCH_RISCV_BITS_H
+#define __ARCH_RISCV_BITS_H
+
+#define BIT(nr)			(1UL << (nr))
+#define BIT_ULL(nr)		(1ULL << (nr))
+#define BIT_MASK(nr)		(1UL << ((nr) % BITS_PER_LONG))
+#define BIT_WORD(nr)		((nr) / BITS_PER_LONG)
+#define BIT_ULL_MASK(nr)	(1ULL << ((nr) % BITS_PER_LONG_LONG))
+#define BIT_ULL_WORD(nr)	((nr) / BITS_PER_LONG_LONG)
+#define BITS_PER_BYTE		8
+
+#define GENMASK_INPUT_CHECK(h, l) 0
+
+#define __GENMASK(h, l) \
+	(((~UL(0)) - (UL(1) << (l)) + 1) & \
+	 (~UL(0) >> (BITS_PER_LONG - 1 - (h))))
+#define GENMASK(h, l) \
+	(GENMASK_INPUT_CHECK(h, l) + __GENMASK(h, l))
+
+
+#endif /* __ARCH_RISCV_BITS_H */
